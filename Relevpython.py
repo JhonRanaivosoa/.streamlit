d787d060584520bba6290be87a35f92d2c5f3afa
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-06-01 00:22:57 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 
 # Titre de l'application
-st.title("Visualisation de données")
+st.title("Visualisation de données par jhon Ranaivosoa")
 
 # Charger les données
 data = [1,2,3,4,5,6]
