29af55b2e108f7c5f44c0f29ef085a401c892c33
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-06-09 19:42:44 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -1,35 +1,15 @@
 import streamlit as st
-import requests
+import gdown
 import pandas as pd
 
 # Lien partagé du fichier sur Google Drive
-file_id = '1ntNnU53yyZg9V5Gt-wwctEIoMLX3c-L-'
 url = 'https://drive.google.com/file/d/1ntNnU53yyZg9V5Gt-wwctEIoMLX3c-L-/view?usp=drive_link'
-
-# Fonction pour télécharger le fichier depuis Google Drive
-def download_file_from_google_drive(url, output):
-    session = requests.Session()
-    response = session.get(url, stream=True)
-    token = None
-
-    for key, value in session.cookies.items():
-        if key.startswith('download_warning'):
-            token = value
-
-    if token:
-        params = {'id': file_id, 'confirm': token}
-        response = session.get(url, params=params, stream=True)
-
-    with open(output, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
 
 # Télécharger le fichier dans un répertoire temporaire
 output = 'file.csv'  # Nom du fichier de sortie
 
 with st.spinner('Téléchargement du fichier...'):
-    download_file_from_google_drive(url, output)
+    gdown.download(url, output, quiet=False)
 
 st.success('Fichier téléchargé avec succès !')
 
