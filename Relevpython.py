ff83ddedf19062f9a5cf115e64bc8497b7267601
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-06-09 22:19:11 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -1,14 +1,35 @@
 import streamlit as st
 
-def main():
-    st.title('Exemple Streamlit avec HTML et Bootstrap')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-    # Lecture du fichier HTML
-    with open('index.html', 'r', encoding='utf-8') as file:
-        html_content = file.read()
+<body>
 
-    # Affichage du contenu HTML
-     st.write(html_content, unsafe_allow_html=True)
+    <div class="container">
+    <h1>Bootstrap Example</h1>
+    <p>This is a simple Bootstrap example.</p>
+    
+    <!-- Example Bootstrap components -->
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+    
+    <button type="button" class="btn btn-primary">Primary Button</button>
+    </div>
 
-if __name__ == '__main__':
-    main()
+  <!-- Bootstrap JS and dependencies (optional) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+    integrity="sha384-FJQpvrou5b1kUK2s4QDycr1fknqtp2pNVa+5tZt6vaw4vZv7vWkzE6bSaJm9KkK"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh/jFcIk5O+r4G/CpJw/TaF0JwJ8ERdknLPMO"
+    crossorigin="anonymous"></script>
+</body>
+</html>
