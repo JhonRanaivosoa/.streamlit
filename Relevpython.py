be25d089d7c76974ccf450bd67b7d34867d0b8bb
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-07-17 15:28:04 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 # Remplacer 'votre_fichier.csv' par le nom de votre fichier CSV
-file_path = 'C:\Users\jhon.treso\OneDrive - Sanifer\Bureau\15\00811820101_1721109524739.csv'
+file_path = 'C:\\Users\\jhon.treso\\OneDrive - Sanifer\\Bureau\\15\\00811820101_1721109524739.csv'
+
 
 # Lire le fichier CSV
 df = pd.read_csv(file_path)
