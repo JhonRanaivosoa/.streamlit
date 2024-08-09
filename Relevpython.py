dbc1b04f52b7cdbaa8b49cdb0554aa6498c496d7
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-08-09 16:50:35 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -3,17 +3,7 @@ import pandas as pd
 import plotly.graph_objs as go
 import hydralit_components as hc
 
-menu_data = [
-    {'label':"Left End"},
-    {'label':"Book"},
-    {'label':"Component"},
-    {'label':"Dashboard"},
-    {'label':"Right End"},
-]
 
-menu_id = hc.nav_bar(menu_definition=menu_data)
-
-st.info(f"{menu_id=}")
 # URL brute du fichier Excel sur GitHub
 excel_url = "https://raw.githubusercontent.com/JhonRanaivosoa/streamlittest/5d21b841763b7f5e76e4e2ffbebc35193d81248a/RELEVE%20SANIFER%202024.xlsx"
 
