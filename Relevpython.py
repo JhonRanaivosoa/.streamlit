8d5bd1c6cc1976a93af7aeb55bfbe14c7c798d0c
JhonRanaivosoa <123720712+JhonRanaivosoa@users.noreply.github.com>
2024-08-09 14:33:30 +0300

    Relevpython.py

--- Relevpython.py
+++ Relevpython.py
@@ -1,24 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-# URL du fichier Excel sur GitHub
-excel_url = "https://raw.githubusercontent.com/votre-utilisateur/votre-repo/main/RELEVE%20SANIFER%202024/RELEVE%20SANIFER%202024.xlsx"
+# URL brute du fichier Excel sur GitHub
+excel_url = "https://raw.githubusercontent.com/JhonRanaivosoa/streamlittest/5d21b841763b7f5e76e4e2ffbebc35193d81248a/RELEVE%20SANIFER%202024.xlsx"
 
-# Fonction pour charger les données du fichier Excel
+# Fonction pour charger les données de la feuille spécifiée
 @st.cache
-def load_data(url):
-    return pd.read_excel(url, sheet_name=None)  # `sheet_name=None` charge toutes les feuilles
+def load_data(url, sheet_name):
+    return pd.read_excel(url, sheet_name=sheet_name)
 
-# Charger les données
-data = load_data(excel_url)
+# Charger les données de la feuille "MCB SANIFER"
+data = load_data(excel_url, "MCB SANIFER")
 
-# Afficher les noms des feuilles dans l'application Streamlit
-st.title("Données du fichier Excel")
-st.write("Feuilles disponibles :")
-for sheet_name in data.keys():
-    st.write(f"- {sheet_name}")
-
-# Afficher les données de la première feuille
-first_sheet = list(data.keys())[0]
-st.write(f"**Données de la feuille : {first_sheet}**")
-st.write(data[first_sheet])
+# Afficher les données dans l'application Streamlit
+st.title("Données de la feuille 'MCB SANIFER'")
+st.write(data)
